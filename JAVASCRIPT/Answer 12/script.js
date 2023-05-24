51ae25b4dde512fbe14250a6868c0fab8e035df9
kiran madiwalar <kiranm81040@gmail.com>
2023-05-24 08:47:18 +0530

    blog app finished

--- JAVASCRIPT/Answer 12/script.js
+++ JAVASCRIPT/Answer 12/script.js
@@ -1,16 +1,4 @@
 const container = document.querySelector(".blog-container");
-
-// const fistBlog = document.querySelector(".blog-1")
-// const secondBlog = document.querySelector(".blog-2")
-// const thirdBlog = document.querySelector(".blog-3")
-// const fourthBlog = document.querySelector(".blog-4")
-// const fifthBlog = document.querySelector(".blog-5")
-// const sixthBlog = document.querySelector(".blog-6")
-// const seventhBlog = document.querySelector(".blog-7")
-// const eighthBlog = document.querySelector(".blog-8")
-// const ninthBlog = document.querySelector(".blog-9")
-// const tenthBlog = document.querySelector(".blog-10")
-
 const blogArr = [];
 
 const onPageLoad = async () => {
@@ -30,7 +18,6 @@ const onPageLoad = async () => {
       <p class="text-center text-[#14213d]">${element.body}</p>
     </div>`
     );
-    // <button class="!btn bg-[#e07a5f] px-4 py-2 rounded text-white hover:bg-[#be6147] transition-all duration-200" id="btn">Delete</button>
   });
 };
 onPageLoad();
@@ -60,7 +47,7 @@ const addData = async (event) => {
     `
   <div class="w-[250px] min-h-[250px] flex flex-col justify-between items-center bg-[#ffffff] p-4 rounded-md drop-shadow-md">
       <h2 class="text-[#fca311] font-serif text-xl text-center">${title}</h2>
-      <p class="text-center text-[#14213d]">${text}</p>
+      <p class="absolute top-[50px] left-[25px]">${text}</p>
     </div>
   `
   );
